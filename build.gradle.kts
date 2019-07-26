2fcb858db2df318f6ed9b38cde4dc516314a2f64
Adam Bertrand <adam.bertrand@accenture.com>
2019-07-26 14:20:43 +0200

    Remove Tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   kotlin("jvm") version "1.3.41"
-  groovy
   application
 }
 
-val junitVersion by extra("5.5.1")
-val spockVersion by extra("1.3-groovy-2.5")
 val ktorVersion by extra("1.2.2")
 val koinVersion by extra("2.0.1")
 group = "com.accenture.cfe.dev"
@@ -22,14 +19,6 @@ dependencies {
   implementation(kotlin("stdlib-jdk8"))
   implementation("io.ktor:ktor-server-netty:$ktorVersion")
   implementation("org.koin:koin-ktor:$koinVersion")
-
-  testImplementation("org.spockframework:spock-core:$spockVersion")
-  testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-  testRuntimeOnly("org.junit.vintage:junit-vintage-engine:$junitVersion")
-}
-
-tasks.withType<Test> {
-  useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
